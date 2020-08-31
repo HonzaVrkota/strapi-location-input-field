cea23c1359191c2bedd50bc0eed16d975f4c7c52
Jan Vrkota <jan@vrkota.name>
2020-08-31 17:00:47 +0200

    Strapi update 3.1.4 - admin user role

--- config/database.js
+++ config/database.js
@@ -4,17 +4,33 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
         useNullAsDefault: true,
-        ssl: false,
       },
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: env("DATABASE_HOST", "127.0.0.1"),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME", "strapi"),
+//         username: env("DATABASE_USERNAME", ""),
+//         password: env("DATABASE_PASSWORD", ""),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//         ssl: false,
+//       },
+//     },
+//   },
+// });
