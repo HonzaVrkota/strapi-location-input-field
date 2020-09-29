b184bcbb43a40d2bb15eed7e20c271d689eb84ec
HonzaVrkota <jan@vrkota.name>
2020-09-29 17:31:51 +0200

    Company ratings

--- config/server.js
+++ config/server.js
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   url: env("URL", "http://localhost:1337"),
   admin: {
-    url: "/",
-    serveAdminPanel: false,
+    // url: "/",
+    // serveAdminPanel: true,
     auth: {
-      secret: env("ADMIN_JWT_SECRET"),
+      secret: env("ADMIN_JWT_SECRET", "2e36QZ3MQ/6JQiUoSCwmF/1GUdpx+QrThmLeis/xcyn+LgwKFuwL4nRojdcgvRGL1SvAZnTzDgFH9Uopf3Gtvg=="),
     },
   },
 });
