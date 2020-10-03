7a25da622a3532bff2bde87f3e2ec9110b327558
Jan Vrkota <jan@vrkota.name>
2020-10-03 12:44:12 +0200

    Server change

--- config/server.js
+++ config/server.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   url: env("URL", "http://localhost:1337"),
   admin: {
-    // url: "/",
-    // serveAdminPanel: true,
+    url: "/",
+    serveAdminPanel: true,
     auth: {
       secret: env("ADMIN_JWT_SECRET", "2e36QZ3MQ/6JQiUoSCwmF/1GUdpx+QrThmLeis/xcyn+LgwKFuwL4nRojdcgvRGL1SvAZnTzDgFH9Uopf3Gtvg=="),
     },
